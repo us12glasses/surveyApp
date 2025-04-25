533f7657f1b410c11a60311b0d584f943ef81704
Faiqul Ghozi <faiqullghz@gmail.com>
2025-04-26 06:17:14 +0700

    commenting unused export button

--- resources/views/vendor/backpack/crud/buttons/export-responses.blade.php
+++ resources/views/vendor/backpack/crud/buttons/export-responses.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ backpack_url('export-responses') }}" class="btn btn-primary">
+<!-- <a href="{{ backpack_url('export-responses') }}" class="btn btn-primary">
     <i class="la la-download"></i> Export to Excel
-</a>
+</a> !-->
